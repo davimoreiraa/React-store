fb442924435f0823cbe506f2a7dd250aceae2601
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-08-13 22:29:28 -0300

    implemented the logo img and update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,17 @@
 /* ------------ STYLES ------------ */
 import styles from './styles.module.css'
 
+/* ------------ COMPONENTS ------------ */
+import Image from 'next/Image'
+import Logo from '../../../public/images/store_icon.png'
+
+
 export default function Header() {
   return (
     <header className={`${styles.header}`}>
-
+      <div className={`${styles.logo_container}`}>
+        <Image src={Logo} layout='responsive' />
+      </div>
     </header>
   )
 }
