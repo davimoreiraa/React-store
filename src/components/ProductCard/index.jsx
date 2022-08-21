f7b805a1a93e5e77c2f32b0bcfbd8472853c1fe8
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-08-21 19:42:40 -0300

    added some more product imgs

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -4,9 +4,13 @@ import styles from './styles.module.css'
 /* ------------ COMPONENTS ------------ */
 import Image from 'next/Image'
 
+
 export default function ProductCard({name, type, price, img}) {
   return (
     <article className={`${styles.card} d-inline-flex rounded-3`}>
+      <div className={`${styles.img_container}`}>
+        <img src={`../../../images/${img}`} className={styles.img}/>
+      </div>
       <h1>{name}</h1>
     </article>
   )
