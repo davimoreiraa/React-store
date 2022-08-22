ecc61ceae4fd944c49a875c03f953f8a63d6c8a3
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-08-21 22:47:47 -0300

    update prodcuct card and home

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,17 +1,15 @@
 /* ------------ STYLES ------------ */
 import styles from './styles.module.css'
 
-/* ------------ COMPONENTS ------------ */
-import Image from 'next/Image'
-
-
 export default function ProductCard({name, type, price, img}) {
   return (
-    <article className={`${styles.card} d-inline-flex rounded-3`}>
-      <div className={`${styles.img_container}`}>
-        <img src={`../../../images/${img}`} className={styles.product_img}/>
+    <article className={`${styles.card} col-12 col-sm-8 col-md-5 col-xxl-3 d-flex align-items-center justify-content-center rounded-3`}>
+      <div>
+        <div className={`${styles.img_container} d-flex justify-content-center`}>
+          <img src={`../../../images/${img}`} className={styles.product_img}/>
+        </div>
+        <h1>{name}</h1>
       </div>
-      <h1>{name}</h1>
     </article>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,24 +11,26 @@ import data from './products.json';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Head>
         <title>React Store</title>
       </Head>
       <div className={`${styles.body}`}>
         <Header/>
-        <main className={`${styles.content}`}>
-          {data.map(product => (
-            <ProductCard 
-            key={product.id} 
-            name={product.name} 
-            price={product.price} 
-            type={product.type}
-            img={product.img}
-            />
-          ))}
+        <main className={`${styles.content} d-flex justify-content-center`}>
+          <div className={`${styles.products_container} d-flex justify-content-center col-11 gap-3 row row-cols-md-2 row-cols-lg-3`}>
+            {data.map(product => (
+              <ProductCard 
+              key={product.id} 
+              name={product.name} 
+              price={product.price} 
+              type={product.type}
+              img={product.img}
+              />
+            ))}
+          </div>
         </main>
       </div>
-    </>    
+    </div>    
   )
 }
