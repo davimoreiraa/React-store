722a656c0145d816370a7d1267686432ac23822b
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-08-21 22:28:38 -0300

    update prodcuct card

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -9,7 +9,7 @@ export default function ProductCard({name, type, price, img}) {
   return (
     <article className={`${styles.card} d-inline-flex rounded-3`}>
       <div className={`${styles.img_container}`}>
-        <img src={`../../../images/${img}`} className={styles.img}/>
+        <img src={`../../../images/${img}`} className={styles.product_img}/>
       </div>
       <h1>{name}</h1>
     </article>
