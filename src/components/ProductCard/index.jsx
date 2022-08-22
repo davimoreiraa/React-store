be39a7b5e37658d20ebd99037b453218bf501bc2
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-08-22 18:28:56 -0300

    update productcard

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -3,13 +3,17 @@ import styles from './styles.module.css'
 
 export default function ProductCard({name, type, price, img}) {
   return (
-    <article className={`${styles.card} col-12 col-sm-8 col-md-5 col-xxl-3 d-flex align-items-center justify-content-center rounded-3`}>
-      <div>
-        <div className={`${styles.img_container} d-flex justify-content-center`}>
-          <img src={`../../../images/${img}`} className={styles.product_img}/>
+    <article className={`${styles.card} col-12 col-sm-8 col-md-5 col-xxl-3 rounded-3`}>
+        <div className={`d-flex justify-content-center`}>
+          <div className={`${styles.img_container} d-flex justify-content-center`}>
+            <img src={`../../../images/${img}`} className={styles.product_img}/>
+          </div>
         </div>
-        <h1>{name}</h1>
-      </div>
+        <div className={`${styles.infos}`}>
+          <p className={styles.name}>{name}</p>
+          <p className={styles.type}>{type}</p>
+          <p className={styles.price}>{price}</p>
+        </div>
     </article>
   )
 }
