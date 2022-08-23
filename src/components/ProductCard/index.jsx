8f05f2dbff015a4ddefd575f24e9b287389ce8e7
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-08-23 20:08:25 -0300

    update product card

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,6 +1,9 @@
 /* ------------ STYLES ------------ */
 import styles from './styles.module.css'
 
+/* ------------ ICONS ------------ */
+import Cart from '../../../public/images/cart.svg'
+
 export default function ProductCard({name, type, price, img}) {
   return (
     <article className={`${styles.card} col-12 col-sm-8 col-md-5 col-xxl-3 rounded-3`}>
@@ -9,10 +12,15 @@ export default function ProductCard({name, type, price, img}) {
             <img src={`../../../images/${img}`} className={styles.product_img}/>
           </div>
         </div>
-        <div className={`${styles.infos}`}>
-          <p className={styles.name}>{name}</p>
-          <p className={styles.type}>{type}</p>
-          <p className={styles.price}>{price}</p>
+        <div className={`d-flex`}>
+          <div className={`${styles.infos}`}>
+            <p className={styles.name}>{name}</p>
+            <p className={styles.type}>{type}</p>
+            <p className={styles.price}>{price}</p>
+          </div>
+        <div>
+          <button className={`${styles.cart_btn}`}> <Cart/> </button>
+        </div>
         </div>
     </article>
   )
