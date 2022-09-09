73e01b5de54ce3199d9c87a6cb45327135543590
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-09-09 20:27:40 -0300

    update

--- src/components/MiniCard/index.jsx
+++ src/components/MiniCard/index.jsx
@@ -14,15 +14,13 @@ export default function MiniCard({ product, removeProduct }) {
   let [products, setProducts] = useContext(Products);
 
   function removeProduct(id) {
-    // let newProductsArray = products
-    // for(var i = 0; i < products.length; i++) { 
-    //   if(products[i].id === id) { 
-    //     newProductsArray.splice(i, 1); 
-    //   }
-    // }
-    // setProducts(newProductsArray)
-    setProducts([...products, id])
-    console.log(products)
+     let newProductsArray = products
+     for(var i = 0; i < products.length; i++) { 
+       if(products[i].id === id) { 
+         newProductsArray.splice(i, 1); 
+       }
+     }
+    setProducts(newProductsArray)
   }
 
 
@@ -35,7 +33,7 @@ export default function MiniCard({ product, removeProduct }) {
         <h1 className={`${styles.product_name} `}>{product.name}</h1>
         <p className={`${styles.price}`}>{product.price}</p>
       </div>
-      <button className={`btn`} onClick={() => removeProduct(product)}>
+      <button className={`btn`} onClick={() => removeProduct(product.id)}>
         <i className={`${styles.trash_icon} align-self-end`}><BsFillTrashFill /></i>
       </button>
     </div>
