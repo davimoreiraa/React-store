1a84c8c5d73194230a493f9f7b41b395a65b2e4f
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-09-16 20:23:33 -0300

    update cartbox to be responsible

--- src/components/CartBox/index.jsx
+++ src/components/CartBox/index.jsx
@@ -23,11 +23,14 @@ export default function CartBox({ showCart }) {
  }
 
   return (
-    <div className={`${styles.card_box} ${showCart ? '' : 'd-none'} position-absolute end-0 col-3 d-flex justify-content-center`}>
+    <div className={`${styles.card_box} ${showCart ? '' : 'd-none'} position-fixed end-0 col-12 col-md-4 col-xl-3 d-flex justify-content-center`}>
       <div className={`col-12`}>
         {products.map((product) => (
           <MiniCard key={product.id} product={product} removeProduct={removeProduct}/>
         ))}
+        <div className={`${styles.total_price_box} rounded-3 d-flex justify-content-center`}>
+          Total: R$ 99999999
+        </div>
       </div>
     </div>
   )
