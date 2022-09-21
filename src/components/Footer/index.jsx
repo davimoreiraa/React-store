3f11559ca045f77c27edf099b905b625bb1915d4
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-09-20 22:32:16 -0300

    update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,21 @@
+/* ------------ STYLES ------------ */
+import styles from './styles.module.css'
+
+/* ------------ COMPONENTS ------------ */
+import Image from 'next/Image'
+
+/* ------------ IMAGES AND ICONS ------------ */
+import Logo from '../../../public/images/store_icon.png'
+
 export default function Footer() {
   return (
-    <footer>
-      
+    <footer className={`${styles.footer}`}>
+      <div className={`d-flex align-items-center`}>
+        <div className={`${styles.logo_container} `}>
+          <Image src={Logo} layout='responsive' />
+        </div>
+        <p className={`${styles.liscense}`}>@React Store</p>
+      </div>
     </footer>
   )
 }
