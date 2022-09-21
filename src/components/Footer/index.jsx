0dee32a2d9477a5b403d7e896af060ca9fc282ff
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-09-21 16:19:11 -0300

    update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,12 +9,36 @@ import Logo from '../../../public/images/store_icon.png'
 
 export default function Footer() {
   return (
-    <footer className={`${styles.footer}`}>
+    <footer className={`${styles.footer} d-lg-flex justify-content-between`}>
       <div className={`d-flex align-items-center`}>
         <div className={`${styles.logo_container} `}>
           <Image src={Logo} layout='responsive' />
         </div>
-        <p className={`${styles.liscense}`}>@React Store</p>
+        <p className={`${styles.liscense}`}>@React Store, Inc</p>
+      </div>
+      <div className={`d-flex align-items-center justify-content-center`}>
+        <nav className={`${styles.nav} d-flex col-7 col-lg-auto align-items-center justify-content-between`}>
+          <ul className={`d-flex align-items-center col-12 justify-content-between`}>
+            <li className={`${styles.nav_item}`}>
+              <a href="">Termos</a>
+            </li>
+            <li className={`${styles.nav_item}`}>
+              <a href="">Licença</a>
+            </li>
+            <li className={`${styles.nav_item}`}>
+              <a href="">API</a>
+            </li>
+            <li className={`${styles.nav_item}`}>
+              <a href="">Segurança</a>
+            </li>
+            <li className={`${styles.nav_item}`}>
+              <a href="">Contato</a>
+            </li>
+            <li className={`${styles.nav_item}`}>
+              <a href="">Sobre</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </footer>
   )
